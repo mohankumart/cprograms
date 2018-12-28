c04091a0152e4e5afeac0d61f8f4d2b208bf845c
Mohan Kumar <tmk_mohankumar@yahoo.com>
2018-12-28 21:07:08 +0530

    6.3.c

---
+++ assignment6/3.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<string.h>
+void swap(char *p, char*q){
+	char temp;
+	temp = *p;
+	*p = *q;
+	*q = temp;	
+}
+
+int checkAsciiCodes(char *str){
+	if((*str >= 77 && *str <= 90)  || (*str >= 97 && *str <= 122)){
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+
+void remove_non_characters(char *str){
+	int len = strlen(str), i,j, flag = 0;
+	char temp;
+	for(i=0; i<len; i++){
+		if(!checkAsciiCodes(str+i)){
+			j = i+1;
+			flag = 0;
+			while(j<len){	
+				if(!checkAsciiCodes(str+j)){
+					j++;
+				}else{
+					swap(str+i, str+j);
+					flag = 1;
+					break;	
+				}		
+			}
+			if(!flag){
+				*(str+i) = '\0';
+			}	
+		}
+	}
+	printf("%s\n", str);
+}
+
+int main(){
+	char string[20];
+	printf("Enter the string: ");		
+	scanf("%s", string);
+	remove_non_characters(string);
+	return 0;
+}
