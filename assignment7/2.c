9d2bc1c45052bfdbcaafbc7ba61a805c675af40f
Mohan Kumar <tmk_mohankumar@yahoo.com>
2018-12-30 22:15:22 +0530

    7.2.c

--- assignment7/2.c
+++ assignment7/2.c
@@ -1,5 +1,19 @@
 #include<stdio.h>
-int main(){
+#include<stdlib.h>
+#include<string.h>
 
+int main(int argc, char const *argv[]){
+	char *str = (char *)malloc(100*sizeof(char));
+	strcpy(str, "wc ");
+	strcat(str, argv[1]);
+	
+	if(argc > 1){
+		FILE *fp = fopen(argv[1], "r");
+		if(fp != NULL){
+			system(str);
+		}
+		fclose(fp);
+	}
+	
 	return 0;
 }
